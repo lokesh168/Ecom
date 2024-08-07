13218e28e172c7306471ef10e928d91755f9ec79
lokesh168 <lokesh.vervali@gmail.com>
2024-08-07 13:49:17 +0530

    add our collection page & footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import { MapPin, Phone, Mail } from "lucide-react";
+
+const Footer = () => {
+    return (
+        <footer className="my-5 py-10 bg-[#F4F4F4] px-14">
+            <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-4 grid-rows-1 gap-4">
+                <div>
+                    <h2 className="text-2xl mb-5">Collections</h2>
+                    <ul className="space-y-2 cursor-pointer">
+                        <li>Black Tea</li>
+                        <li>Green Tea</li>
+                        <li>Soft Tea</li>
+                        <li>Hard Tea</li>
+                        <li>Crispy Tea</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="text-2xl mb-5">Learn</h2>
+                    <ul className="space-y-2 cursor-pointer">
+                        <li>About Us</li>
+                        <li>About Our Teas</li>
+                        <li>Tea Academy Tea</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="text-2xl mb-5">Customer Service</h2>
+                    <ul className="space-y-2 cursor-pointer">
+                        <li>Ordering and payment </li>
+                        <li>Delivery</li>
+                        <li>Privacy and policy </li>
+                        <li>Hard Tea</li>
+                        <li>Terms & Conditions</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="text-2xl mb-5">Contact Us</h2>
+                    <ul className="space-y-3 cursor-pointer">
+                        <li className="flex space-x-5">
+                            <MapPin size={34} />
+                            <p>
+                                3 Falahi, Falahi St, Pasdaran Ave, Shiraz, Fars
+                                Provieence Iran
+                            </p>
+                        </li>
+                        <li className="flex space-x-5">
+                            <Mail size={20} />
+                            <p>Email: amoopur@gmail.com</p>
+                        </li>{" "}
+                        <li className="flex space-x-5">
+                            <Phone size={20} />
+                            <p>Tel: +98 9173038406 </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Collections.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+
+const Collections = () => {
+    const [receipe, setReceipe] = useState();
+    const [dishNames, setDishName] = useState();
+    const [filteredList, setFilteredList] = useState();
+
+    function removeDuplicates(arr) {
+        let unique = arr.reduce(function (acc, curr) {
+            if (!acc.includes(curr.cuisine)) acc.push(curr.cuisine);
+            return acc;
+        }, []);
+        return unique;
+    }
+
+    function receipeNameHandler(receipeName) {
+        const output = receipe?.recipes.filter((item) => {
+            if (item.cuisine == receipeName) return item;
+        });
+
+        setFilteredList(output);
+    }
+
+    async function fetchRestApi() {
+        const uri = await fetch("https://dummyjson.com/recipes");
+
+        const response = await uri.json();
+        setDishName(removeDuplicates(response?.recipes));
+        setReceipe(response);
+    }
+
+    useEffect(() => {
+        fetchRestApi();
+    }, []);
+
+    console.log(filteredList);
+
+    return (
+        <section>
+            {/* Banner */}
+            <div>
+                <img
+                    className="h-60 w-full object-cover"
+                    src="https://plus.unsplash.com/premium_photo-1673484470265-39526c43ca3e?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="banner"
+                />
+            </div>
+
+            {/* Collections */}
+            <div>
+                <h1 className="text-center my-10 text-4xl">Our Collection</h1>
+
+                <div className="grid grid-cols-5 grid-rows-5 gap-4 p-5">
+                    <div className="col-span-1 row-span-5 flex flex-col">
+                        {dishNames?.map((item, index) => (
+                            <button
+                                key={index}
+                                onClick={() => receipeNameHandler(item)}
+                                className="my-1 p-4 border-b-2 border-black hover:bg-[#AF8260]"
+                            >
+                                {item}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="col-span-4 row-span-5 col-start-2 relative">
+                        <button className="my-1 p-4 border-b-2 border-black hover:bg-[#AF8260] absolute top-[-8%] right-0">
+                            <p className="text-right">Sort</p>
+                        </button>
+                        <div className="container flex flex-wrap mx-auto">
+                            {filteredList.map((item, index) => (
+                                <div
+                                    className="mx-5 text-center w-64 mb-10 hover:bg-[#d3d3d3] transition"
+                                    key={index}
+                                >
+                                    <img
+                                        className="rounded"
+                                        src={item.image}
+                                        alt="something about tea"
+                                    />
+                                    <p className="uppercase mt-5 font-bold text-md">
+                                        {item.name}
+                                    </p>
+                                    <p className="uppercase font-bold text-sm py-2">
+                                        $ {Math.floor(Math.random() * 200)}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Footer */}
+            <Footer />
+        </section>
+    );
+};
+
+export default Collections;
